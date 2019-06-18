9d819bdd8011d491f9db14260d48858eb41fd1cd
artisanlabs <artisan254@gmail.com>
2019-06-18 18:27:48 +0300

    Added the b2c request handler

--- src/Payment.php
+++ src/Payment.php
@@ -25,4 +25,20 @@ class Payment {
 			throw new \Exception($exception->getMessage());
 		}
 	}
+
+	/**
+	 * Generate a new b2c request
+	 * @param int $phoneNumber
+	 * @param int $amount
+	 * @param string $referenceCode
+	 * @return array
+	 * @throws \Exception
+	 */
+	public function b2c(int $phoneNumber, int $amount, string $referenceCode) {
+		try {
+			return (array)(new PaymentController())->initiateB2CTransaction($phoneNumber, $amount, $referenceCode);
+		} catch (\Exception $exception) {
+			throw new \Exception($exception->getMessage());
+		}
+	}
 }
