122b64ac016cc0c1b785d05d14cb7eda9527ad10
artisanlabs <artisan254@gmail.com>
2019-06-19 11:06:44 +0300

    Renamed the routes

--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,8 @@ Route::group([
 	'middleware' => 'signed',
 ], function () {
 	// STK Callback
-	Route::post('stk-callback', 'CallbackController@stkCallback')->name('payment.stk.callback');
+	Route::post('stk', 'CallbackController@stkCallback')->name('payment.stk.callback');
 	// B2C Callback
-	Route::post('stk-callback', 'CallbackController@b2cCallback')->name('payment.b2c.callback');
+	Route::post('b2c', 'CallbackController@b2cCallback')->name('payment.b2c.callback');
 
 });
